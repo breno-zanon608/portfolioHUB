832b594715c08e5078ec7522219b62295703616c
Breno Zanon de Sousa <breno.zanon07@gmail.com>
2026-04-11 00:06:28 -0300

    Create conversor_temperatura.c

---
+++ conversor_temperatura.c
@@ -0,0 +1,12 @@
+
+  #include <stdio.h>
+
+int main() {
+  float tc;
+  float tf;
+  printf("Digite um valor em Celsius: ");
+  scanf("%f" , &tc);
+  tf = (9*tc +160) / 5 ;
+  printf("A temperatura em escala absoluta é: %f" , tf);
+
+}
